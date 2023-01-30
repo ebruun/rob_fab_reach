81a5e5cfe2bd7adb160a0859e9fb7f034c06d374
EdvardBruun <ebruun@princeton.edu>
2023-01-30 15:37:18 -0500

    finished 96-100 for rob1

--- reachability.py
+++ reachability.py
@@ -334,15 +334,24 @@ if __name__ == "__main__":
     calc = True
     merge = False
 
-    rob_nums = ["rob3"]
-    planning_groups = ["robot3_gripper"]
+    rob_nums = [
+        "rob1",
+        # "rob2",
+        # "rob3"
+        ]
+
+    planning_groups = [
+        "robot1_track_gripper",
+        # "robot2_track_gripper",
+        # "robot3_gripper"
+        ]
 
     for rob_num, planning_group in zip(rob_nums,planning_groups):
 
         path = get_directory("_data", rob_num)
 
         if calc:
-            skip_rng = range(0, 20)
+            skip_rng = range(0, 95)
             robot = connect_and_scene()
             main(robot, rob_num, planning_group, path, skip_rng)
 
