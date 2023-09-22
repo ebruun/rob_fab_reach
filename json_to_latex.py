8c0d2db2c1b248b0d80f1ff706d2037ec4a0ac0f
EdvardBruun <ebruun@princeton.edu>
2023-09-22 16:19:36 -0400

    regen all the latex files

--- json_to_latex.py
+++ json_to_latex.py
@@ -7,7 +7,6 @@ from collections import OrderedDict
 def generate_filename(a, s, r):
     name = "a{:0>3}_s{:0>2}_r{:0>3}"
 
-    # angle = str(a).split(".")[0]
     angle = str(int(a * 10))
     span = str(s).replace(".", "")
     ratio = str(int(round(r * 100, 0))).replace(".", "")
@@ -84,38 +83,35 @@ def read_json_data_reach(files_in, txt):
             ratio = data_in["arch_info"]["ratio"]
 
             reach_ratio_coop = data_in["reach_data_coop"]["reach_ratio"]
-            reach_ratio_single = data_in["reach_data_single"]["reach_ratio"]
-            # avg_reach_score_coop = data_in["reach_data_coop"]["avg_reach_score"]
-            avg_reach_score_single = data_in["reach_data_single"]["avg_reach_score"]
+            # reach_ratio_single = data_in["reach_data_single"]["reach_ratio"]
+            avg_reach_score_coop = data_in["reach_data_coop"]["avg_reach_score"]
+            # avg_reach_score_single = data_in["reach_data_single"]["avg_reach_score"]
 
             if i % 6 == 0:
                 lines_data.append(txt[0])
                 lines_data.append(txt[1].format(ratio))
 
-            if reach_ratio_single < 0.99 and reach_ratio_coop < 0.99:
+            if reach_ratio_coop < 0.80:
                 lines_data.append(
                     txt[3].format(
                         "lightred",
-                        reach_ratio_single * 100,
                         reach_ratio_coop * 100,
-                        avg_reach_score_single,
+                        avg_reach_score_coop,
                     )
                 )
-            elif reach_ratio_single == 1.0 and reach_ratio_coop < 0.99:
+            elif reach_ratio_coop < 0.99:
                 lines_data.append(
                     txt[3].format(
                         "lightyellow",
-                        reach_ratio_single * 100,
                         reach_ratio_coop * 100,
-                        avg_reach_score_single,
+                        avg_reach_score_coop,
                     )
                 )
             else:
                 lines_data.append(
                     txt[2].format(
-                        reach_ratio_single * 100,
                         reach_ratio_coop * 100,
-                        avg_reach_score_single,
+                        avg_reach_score_coop,
                     )
                 )
 
@@ -210,15 +206,15 @@ paths = [
 
 force_limit = [70, 235]  # IRB5710-70/2.70  # IRB6700-235/2.65
 
-angles = [0, 15, 30, 45]
+angles = [0, 7.5, 15, 22.5]
 spans = [2.0, 3.0, 4.0, 5.0, 6.0, 7.0]
-ratios = [0.25, 0.50, 0.75, 1.0, 1.5, 2.0]
+ratios = [0.25, 0.50, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0]
 
 txt = [
     "\\\\[-0.2em]\cmidrule{2-8}\\\\[-1.3em]",
     "&\multicolumn{{1}}{{c|}}{{\makecell[lc]{{\\\\{:.2f}}}}}",
-    "&\\tiny{{\makecell[lc]{{S:{:.0f}\\%\\\\C:{:.0f}\\%\\\\fab:{}}}}}",
-    "&\\tiny\cellcolor{{{}}}{{\makecell[lc]{{S:{:.0f}\\%\\\\C:{:.0f}\\%\\\\fab:{}}}}}",
+    "&\\tiny{{\makecell[lc]{{C:{:.0f}\\%\\\\fab:{}}}}}",
+    "&\\tiny\cellcolor{{{}}}{{\makecell[lc]{{C:{:.0f}\\%\\\\fab:{}}}}}",
     "&\\tiny{{\makecell[lc]{{max:{:.0f}\\\\75\\%:{:.0f}\\\\50\\%:{:.0f}}}}}",
     "&\\tiny\cellcolor{{{}}}{{\makecell[lc]{{max:{:.0f}\\\\75\\%:{:.0f}\\\\50\\%:{:.0f}}}}}",
     "&\\footnotesize{{\makecell[lc]{{n={}\\\\{:.0f}kg}}}}",
@@ -226,9 +222,9 @@ txt = [
 
 # generate these files
 file_out = [
-    "_a{:0>2}_reachability_latex.txt",
-    "_a{:0>2}_force_latex.txt",
-    "_a{:0>2}_info_latex.txt",
+    "_a{:0>3}_reachability_latex.txt",
+    "_a{:0>3}_force_latex.txt",
+    "_a{:0>3}_info_latex.txt",
 ]
 
 
@@ -236,7 +232,7 @@ file_out = [
 # RUN CODE
 ##################
 
-path_idx = 1  # 0 = track, 1 = no_track
+path_idx = 0  # 0 = track, 1 = no_track
 angle_idxs = [0, 1, 2, 3]
 
 files_in = generate_filepaths(paths[path_idx], angles[0], spans, ratios)
@@ -245,7 +241,7 @@ files_in = generate_filepaths(paths[path_idx], angles[0], spans, ratios)
 # GENERATE INFO FILE (same for all angles)
 lines_data = read_json_data_info(files_in, txt)
 lines_data.pop(0)  # remove 1st line
-file_out_save = file_out[2].format(angles[0])
+file_out_save = file_out[2].format(int(angles[0] * 10))
 write_text_data(paths[path_idx], file_out_save, lines_data)
 
 # GENERATE REACH & FORCE FILES (same for all angles)
@@ -254,10 +250,10 @@ for angle_idx in angle_idxs:
 
     lines_data = read_json_data_reach(files_in, txt)
     lines_data.pop(0)  # remove 1st line
-    file_out_save = file_out[0].format(angles[angle_idx])
+    file_out_save = file_out[0].format(int(angles[angle_idx] * 10))
     write_text_data(paths[path_idx], file_out_save, lines_data)
 
     lines_data = read_json_data_force(files_in, txt, force_limit[path_idx])
     lines_data.pop(0)  # remove 1st line
-    file_out_save = file_out[1].format(angles[angle_idx])
+    file_out_save = file_out[1].format(int(angles[angle_idx] * 10))
     write_text_data(paths[path_idx], file_out_save, lines_data)
